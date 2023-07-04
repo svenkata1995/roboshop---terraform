5d393388a66de6a30e436e22b5a4c19ac6c2bfed
svenk <svenkata1995@gmail.com>
2023-07-04 18:42:24 +0530

    roboshop---terraform

--- servers.tf
+++ servers.tf
@@ -1,11 +1,4 @@
 
 
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z0776773NTS6LV55SFCI"
-  name    = "frontend-dev.rdevopsb72.tech"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
 
 
