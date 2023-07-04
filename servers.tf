9c28d5640d3995b59e29d36fcdf5101b5b759b28
svenk <svenkata1995@gmail.com>
2023-07-04 16:27:57 +0530

    roboshop---terraform

--- servers.tf
+++ servers.tf
@@ -1,43 +1,10 @@
-resource "aws_instance" "Frontend" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
 
-  tags = {
-    Name = "Frontend"
-  }
-}
 
 resource "aws_route53_record" "frontend" {
   zone_id = Z0776773NTS6LV55SFCI
-  name    = "www.example.com"
+  name    = "frontend-dev.rdevopsb72.tech"
   type    = "A"
   ttl     = 30
   records = [aws_instance.frontend.private_ip]
 }
 
-resource "aws_instance" "Mongodb" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "Mongodb"
-  }
-}
-
-resource "aws_instance" "Catalogue" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-
-resource "aws_instance" "User" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "User"
-  }
-}
